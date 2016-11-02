ccc919d92d7455586de38009efa223f40b5398f2
alexfaunt <alexfaunt@gmail.com>
2016-11-02 11:22:58 +0000

    Preact compat test case

---
+++ server.jsx
@@ -0,0 +1,7 @@
+// Can import react directly with no problem
+import react from 'react'
+
+// Cant import react-router which depends on react
+import { Link } from 'react-router'
+
+// Error: Cannot find module 'react'
